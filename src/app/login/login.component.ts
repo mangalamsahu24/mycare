7f936fa62454cdf02ce378e03e014fd808b33656
mangalamsahu24 <sahumangalm24@gmail.com>
2024-03-06 15:10:55 +0530

    log

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router'; // Import the Router
 
 @Component({
   selector: 'app-login',
@@ -10,8 +11,8 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
 
+  constructor(private http: HttpClient, private router: Router) {}
 
-  constructor(private http: HttpClient) {}
   onSubmit() {
     const loginData = {
       username: this.username,
@@ -21,10 +22,17 @@ export class LoginComponent {
     this.http.post('http://localhost:3000/login', loginData)
       .subscribe((response: any) => {
         if (response.success) {
-          console.log('Login successful hello',response.message);
+          console.log('Login successful:', response.message);
+          // Redirect to the home page upon successful login
+          this.router.navigate(['admin']);
         } else {
-          console.log('Login failed: ', response.message);
+          console.log('Login failed:', response.message);
+          alert('Invalid login credentials. Please try again.');
+          // Handle failed login, e.g., show an error message to the user
         }
+      }, (error) => {
+        console.error('Error during login:', error);
+        // Handle other errors, e.g., show an error message to the user
       });
   }
 }
